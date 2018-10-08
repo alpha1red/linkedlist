2689998c00e49600f85c75dd2dcd24ba607d769c
league <league@imac-4.attlocal.net>
2018-10-07 17:40:52 -0700

    added prepend and test

--- src/org/jointheleague/awesome500/linkedlist/LinkedList.java
+++ src/org/jointheleague/awesome500/linkedlist/LinkedList.java
@@ -1,4 +1,4 @@
-package node;
+package org.jointheleague.awesome500.linkedlist;
 
 
 public class LinkedList<T> {
@@ -7,16 +7,26 @@ public class LinkedList<T> {
 	}
 	
 	private Node<T> first = null;
+	private Node<T> last = null;
 	
 	public void append(T element) {
 		if (first == null) {
 			first = new Node<T>(element);
+			last = first;
 		} else {
-			Node<T> last = first;
-			while (last.getNext() != null) {
-				last = last.getNext();
-			}
 			last.setNext(new Node<T>(element));
+			last = last.getNext();
+		}
+	}
+	
+	public void prepend(T element) {
+		if (last == null) {
+			last = new Node<T>(element);
+			first = last;
+		} else {
+			Node<T> newNode = new Node<T>(element); 
+			newNode.setNext(first);
+			first = newNode;
 		}
 	}
 	
--- test2/org/jointheleague/awesome500/linkedlist/TestLinkedList.java
+++ test2/org/jointheleague/awesome500/linkedlist/TestLinkedList.java
@@ -1,14 +1,35 @@
-package stuff;
+package org.jointheleague.awesome500.linkedlist;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+
 
 import org.junit.jupiter.api.Test;
 
 class TestLinkedList {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAppend() {
+		LinkedList<Integer> testList = new LinkedList<> ();
+		testList.append(3);
+		testList.append(9);
+		testList.append(5);
+		
+		assertEquals(Integer.valueOf(3), testList.getFirst().getValue());
+		assertEquals(Integer.valueOf(9), testList.getFirst().getNext().getValue());
+		assertEquals(Integer.valueOf(5), testList.getFirst().getNext().getNext().getValue());
+	}
+	
+	@Test
+	void testPrepend() {
+		LinkedList<Integer> testList = new LinkedList<> ();
+		testList.prepend(3);
+		testList.prepend(9);
+		testList.prepend(5);
+		
+		assertEquals(Integer.valueOf(5), testList.getFirst().getValue());
+		assertEquals(Integer.valueOf(9), testList.getFirst().getNext().getValue());
+		assertEquals(Integer.valueOf(3), testList.getFirst().getNext().getNext().getValue());
 	}
 
 }
